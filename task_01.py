9878060ed60f2527f699513e65ccfa498aed542d
Grace Park <pjeanne14@gmail.com>
2015-11-02 09:20:55 +0000

    Now Lint and Unit pass.

--- task_01.py
+++ task_01.py
@@ -17,28 +17,24 @@ def sum_orders(customers, orders):
     >>> print sum_orders(customers = CUSTOMERS, orders = ORDERS)
     >>>
     {2: {'email': 'email@one.com', 'total': 20, 'orders': 2,
-     'name': 'Person One'}, 3: {'email': 'email@two.com',
-     'total': 15, 'orders': 1, 'name': 'Person Two'}}
+    'name': 'Person One'}, 3: {'email': 'email@two.com',
+    'total': 15, 'orders': 1, 'name': 'Person Two'}}
 
     """
     cdict = {}
-    for customer_id1, info1 in customers.iteritems():
-        ordr_num = 0
-        ordr_total = 0
-        for ordr in orders.itervalues():
-            if customer_id1 == ordr.get('customer_id'):
-                ordr_num += 1
-                ordr_total += ordr.get('total')
-            cdict[customer_id1] = {'name': info1.get('name'),
-                                   'email': info1.get('email'),
-                                   'orders': ordr_num,
-                                   'total': ordr_total}
-        return cdict
-    
+    for ordr in orders.itervalues():
+        if ordr['customer_id'] not in cdict:
+            cdict[ordr['customer_id']] = {'total': ordr['total']}
+            cdict[ordr['customer_id']]['orders'] = 1
+            cdict[ordr['customer_id']].update(customers[ordr['customer_id']])
+        else:
+            cdict[ordr['customer_id']]['total'] += ordr['total']
+            cdict[ordr['customer_id']]['orders'] += 1
+    return cdict
 
-ORDERS = {1: {'customer_id': 2, 'total': 10},
-          3: {'customer_id': 2, 'total': 10},
-          4: {'customer_id': 3, 'total': 15}}
-CUSTOMERS = {2: {'name': 'Person One', 'email': 'email@one.com'},
-             3: {'name': 'Person Two', 'email': 'email@two.com'}}
-print sum_orders(customers=CUSTOMERS, orders=ORDERS)
+# ORDERS = {1: {'customer_id': 2, 'total': 10},
+#          3: {'customer_id': 2, 'total': 10},
+#          4: {'customer_id': 3, 'total': 15}}
+# CUSTOMERS = {2: {'name': 'Person One', 'email': 'email@one.com'},
+#             3: {'name': 'Person Two', 'email': 'email@two.com'}}
+# print sum_orders(customers=CUSTOMERS, orders=ORDERS)
